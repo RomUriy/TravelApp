e7fd52e9f838931e1afbadaa67d2fbbc1686b1d8
Roman Yurkevych <romuriy@gmail.com>
2017-05-21 14:44:00 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# Travel App
+Plan your trip and see the world! Use this app!
+
+## Getting Started
+You can use this app by PhoneGapBuild. This app is available on Android.
+
+## Application Functions:
+1. Create your account;
+2. Adding new events (dreams):
+* destination;
+* date;
+* description
+* tick as done
+3. Amend your trip, delete or tick as done;
+4. Map with your geolocation;
+5. Local weather of geolocation;
+
+## Built With:
+
+* [phonegapbuild](https://build.phonegap.com/apps) - is a cloud-based service built on top of the PhoneGap framework. It allows you to easily build those same mobile apps in the cloud.
+* [ionic](https://ionicframework.com/)- the top open source framework for building amazing mobile apps. 
+* [firebase](https://www.firebase.com) - gives you the tools and infrastructure you need to build better apps and grow successful businesses.
+
+## Authors:
+
+* Roman Yurkevych - Project manager - [RomUriy](https://github.com/RomUriy)
+* Ihor Lobanov - Co-worker - [1obanov](https://github.com/1obanov)
--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -1,6 +1,4 @@
-/**
-* Handles the sign in button press.
-*/
+
 function toggleSignIn() {
   if (firebase.auth().currentUser) {
     // [START signout]
@@ -43,9 +41,7 @@ function toggleSignIn() {
   document.getElementById('quickstart-sign-in').disabled = true;
 }
 
-/**
-* Handles the sign up button press.
-*/
+
 function handleSignUp() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
@@ -77,19 +73,6 @@ function handleSignUp() {
   // [END createwithemail]
 }
 
-/**
-* Sends an email verification to the user.
-*/
-// function sendEmailVerification() {
-//   // [START sendemailverification]
-//   firebase.auth().currentUser.sendEmailVerification().then(function() {
-//     // Email Verification sent!
-//     // [START_EXCLUDE]
-//     alert('Email Verification Sent!');
-//     // [END_EXCLUDE]
-//   });
-//   // [END sendemailverification]
-// }
 
 function sendPasswordReset() {
   var email = document.getElementById('email').value;
@@ -120,53 +103,20 @@ function tryMe() {
   window.location.href='travelapp.html';
 }
 
-// function nextPageSignUp() {
-//   window.location.href='travelapp.html';
-// }
 
-/**
-* initApp handles setting up UI event listeners and registering Firebase auth listeners:
-*  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-*    out, and that is where we update the UI.
-*/
 function initApp() {
-  // Listening for auth state changes.
   // [START authstatelistener]
   firebase.auth().onAuthStateChanged(function(user) {
     // [START_EXCLUDE silent]
-    // document.getElementById('quickstart-verify-email').disabled = true;
-    document.getElementById('try_aplication').disabled = true;
-
+    document.getElementById('try_application').disabled = true;
     // [END_EXCLUDE]
     if (user) {
-      // User is signed in.
-      // var displayName = user.displayName;
-      // var email = user.email;
-      // var emailVerified = user.emailVerified;
-      // var photoURL = user.photoURL;
-      // var isAnonymous = user.isAnonymous;
-      // var uid = user.uid;
-      // var providerData = user.providerData;
-      // [START_EXCLUDE]
-      // document.getElementById('quickstart-sign-in-status').textContent = '';
       document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-      document.getElementById('try_aplication').disabled = false;
-
-      // document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-    
-
-
-
-      // if (!emailVerified) {
-      //   document.getElementById('quickstart-verify-email').disabled = false;
-      // }
-      // [END_EXCLUDE]
+      document.getElementById('try_application').disabled = false;
     } else {
       // User is signed out.
       // [START_EXCLUDE]
-      // document.getElementById('quickstart-sign-in-status').textContent = '';
       document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-      // document.getElementById('quickstart-account-details').textContent = 'null';
       // [END_EXCLUDE]
     }
     // [START_EXCLUDE silent]
@@ -177,10 +127,9 @@ function initApp() {
 
   document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
   document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-  document.getElementById('try_aplication').addEventListener('click', tryMe, false);
-  // document.getElementById('quickstart-sign-up').addEventListener('click', nextPageSignUp, false);
-  // document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
   document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+  document.getElementById('try_application').addEventListener('click', tryMe, false);
+
 }
 
 window.onload = function() {
